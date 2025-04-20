b5f9740ab02d461c31e0ead3f3266df5f3c9c67c
Alok Kumar <alok.kumar@ovyo.com>
2025-04-20 12:02:21 +0530

    Initial Commit

---
+++ DemoKeyboardHandling/DemoKeyboardHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoKeyboardHandlingApp.swift
+//  DemoKeyboardHandling
+//
+//  Created by Alok Kumar on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoKeyboardHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
